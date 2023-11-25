e13632d9013701b43e18e74086e0dc98c908863c
宮副大佑 <miyazoedaisuke@miyafukudaisukenoMacBook-Pro.local>
2023-11-25 23:53:03 +0900

    third

--- home_tasker/models.py
+++ home_tasker/models.py
@@ -8,8 +8,26 @@ class Folder(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-      return self.title
+        return self.title
     
     class Meta:
-      verbose_name = 'フォルダ'
-      verbose_name_plural = 'フォルダ'
+        verbose_name = 'フォルダ'
+        verbose_name_plural = 'フォルダ'
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [(1, '未完了'),(2, '作業中'),(3, '完了')]
+
+    folder_id = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    due_date =  models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'タスク'
+        verbose_name_plural = 'タスク'
--- home_tasker/views.py
+++ home_tasker/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Folder
+from .models import Folder, Task
 
 def index(request, id):
     folders = Folder.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
-    return render(request, 'index.html', {'folders': folders})
+    current_folder = get_object_or_404(Folder, id=id)
+    tasks = Task.objects.filter(folder_id=current_folder.id)
+    return render(request, 'index.html', {
+        'folders': folders,
+        'tasks': tasks,
+        'current_folder_id': current_folder.id,
+        })
